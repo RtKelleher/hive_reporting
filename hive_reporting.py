ccabd74b0dc7ba2a1839a83fb24b9c42f637eb7d
Ryan Kelleher <5748165+RtKelleher@users.noreply.github.com>
2020-07-13 15:05:14 -0400

    Update hive_reporting.py
    updated rows to use static declarations which in turn allows for static panda chart variables vs. trying to count and declare them at runtime.

--- hive_reporting.py
+++ hive_reporting.py
@@ -24,9 +24,9 @@ from thehive4py.api import TheHiveApi
 
 import pandas as pd
 
-API = TheHiveApi("<URI>", "<API>")
-SMTP_SERVER = "<Server Address>"
-SENT_TO = "Email Address"
+API = TheHiveApi("", "")
+SMTP_SERVER = ""
+SENT_TO = ""
 
 
 class SIRPPipeline(object):
@@ -36,14 +36,28 @@ class SIRPPipeline(object):
         TIME_FMT (str): Time format.
         data_frame_INDEX (list): List of desired parsed values from dictionary.
     """
+
     TIME_FMT = "%m/%d/%Y %H:%M:%S"
-    data_frame_INDEX = [
-        'Created', 'Severity', 'Owner', 'Name', 'Closed', 'Resolution'
+    data_frame_INDEX = ["Created", "Severity", "Owner", "Name", "Closed", "Resolution"]
+    counts_frame_INDEX = [
+        "totals",
+        "Team.Member",
+        "Team.Member1",
+        "Team.Member2",
+        "Team.Member3",
+        "Team.Member4",
+        "Team.Member5",
+        "Team.Member6",
+        "Duplicated",
+        "TruePositive",
+        "FalsePositive",
+        int("1"),
+        int("2"),
+        int("3"),
     ]
 
     def __init__(
-            self,
-            api,
+        self, api,
     ):
         """
         Security Incident Response Platform prosessing pipeline.
@@ -82,24 +96,18 @@ class SIRPPipeline(object):
             (obj): Resolution
         """
         days_dict[key] = {
-            "Name":
-            record["title"],
-            "Owner":
-            record["owner"],
-            "Severity":
-            record["severity"],
-            "Created":
-            (time.strftime(SIRPPipeline.TIME_FMT,
-                           time.gmtime(record["createdAt"] / 1000.0)))
+            "Name": record["title"],
+            "Owner": record["owner"],
+            "Severity": record["severity"],
+            "Created": (time.strftime(SIRPPipeline.TIME_FMT, time.gmtime(record["createdAt"] / 1000.0))),
         }
-        if 'endDate' in record:
-            days_dict[key].update({
-                "Closed":
-                (time.strftime(SIRPPipeline.TIME_FMT,
-                               time.gmtime(record["endDate"] / 1000.0))),
-                "Resolution":
-                record["resolutionStatus"]
-            })
+        if "endDate" in record:
+            days_dict[key].update(
+                {
+                    "Closed": (time.strftime(SIRPPipeline.TIME_FMT, time.gmtime(record["endDate"] / 1000.0),)),
+                    "Resolution": record["resolutionStatus"],
+                }
+            )
 
     def _fill_day_dicts(self):
         """Set keys for dictionary based on comparitive EPOCH
@@ -112,12 +120,10 @@ class SIRPPipeline(object):
         """
         today = datetime.date.today()
         for i, record in enumerate(self._dataset):
-            if (record["createdAt"] / 1000) > time.mktime(
-                    (today - datetime.timedelta(days=30)).timetuple()):
+            if (record["createdAt"] / 1000) > time.mktime((today - datetime.timedelta(days=30)).timetuple()):
                 self._add_record(self._all30_dict, record, key=i)
 
-            elif (record["createdAt"] / 1000) > time.mktime(
-                    (today - datetime.timedelta(days=60)).timetuple()):
+            elif (record["createdAt"] / 1000) > time.mktime((today - datetime.timedelta(days=60)).timetuple()):
                 self._add_record(self._all60_dict, record, key=i)
 
             else:
@@ -130,27 +136,20 @@ class SIRPPipeline(object):
             (obj): data_frame_90days
             (obj): data_frame_counts
         """
-        self._data_frame_30days = pd.DataFrame(
-            self._all30_dict, index=SIRPPipeline.data_frame_INDEX).transpose()
-        self._data_frame_60days = pd.DataFrame(
-            self._all60_dict, index=SIRPPipeline.data_frame_INDEX).transpose()
-        self._data_frame_90days = pd.DataFrame(
-            self._all90_dict, index=SIRPPipeline.data_frame_INDEX).transpose()
-        counts = self._data_frame_30days['Severity'].value_counts().to_dict()
-        self._data_frame_counts = pd.DataFrame({
-            'Created': {
-                'total': self._data_frame_30days.count()['Created']
+        self._data_frame_30days = pd.DataFrame(self._all30_dict, index=SIRPPipeline.data_frame_INDEX).transpose()
+        self._data_frame_60days = pd.DataFrame(self._all60_dict, index=SIRPPipeline.data_frame_INDEX).transpose()
+        self._data_frame_90days = pd.DataFrame(self._all90_dict, index=SIRPPipeline.data_frame_INDEX).transpose()
+        self._data_frame_counts = pd.DataFrame(
+            {
+                "Created": {"totals": self._data_frame_30days.count()["Created"]},
+                "Closed": {"totals": self._data_frame_30days.count()["Closed"]},
+                "Owner": (self._data_frame_30days["Owner"].value_counts().to_dict()),
+                "Resolution": (self._data_frame_30days["Resolution"].value_counts().to_dict()),
+                "Severity": (self._data_frame_30days["Severity"].value_counts().to_dict()),
             },
-            'Closed': {
-                'total': self._data_frame_30days.count()['Closed']
-            },
-            'Owner':
-            (self._data_frame_30days['Owner'].value_counts().to_dict()),
-            'Resolution':
-            (self._data_frame_30days['Resolution'].value_counts().to_dict()),
-            'Severity': {k: counts.get(k, 0)
-                         for k in range(1, 4)}
-        })
+            index=self.counts_frame_INDEX,
+        )
+        self._data_frame_counts.fillna(0, inplace=True)
 
     @staticmethod
     def _set_workbook_layout(workbook, worksheet, data_frame):
@@ -171,13 +170,9 @@ class SIRPPipeline(object):
         worksheet.freeze_panes(1, 0)
         worksheet.autofilter("A1:G100")
 
-        header_format = workbook.add_format({
-            "bold": True,
-            "text_wrap": True,
-            "valign": "top",
-            "fg_color": "#CCCCCC",
-            "border": 1,
-        })
+        header_format = workbook.add_format(
+            {"bold": True, "text_wrap": True, "valign": "top", "fg_color": "#CCCCCC", "border": 1}
+        )
 
         for col_num, value in enumerate(data_frame.columns.values, 1):
             worksheet.write(0, col_num, value, header_format)
@@ -192,77 +187,77 @@ class SIRPPipeline(object):
             cell_pos (str): chart position on sheet
             series (func): pandas series
         """
+
         def _insert_pie_chart(wbook, wsheet, title, cell_pos, series):
             piechart = wbook.add_chart({"type": "pie"})
-            piechart.set_title({'name': title})
+            piechart.set_title({"name": title})
             piechart.set_style(10)
             piechart.add_series(series)
-            wsheet.insert_chart(cell_pos, piechart, {
-                'x_offset': 25,
-                'y_offset': 10
-            })
+            wsheet.insert_chart(cell_pos, piechart, {"x_offset": 25, "y_offset": 10})
 
         def _data_frame_days_to_excel(writer, sheet_name, data_frame_days):
-            data_frame_days.to_excel(writer,
-                                     sheet_name=sheet_name,
-                                     startrow=1,
-                                     header=False)
-            self._set_workbook_layout(writer.book, (writer.sheets[sheet_name]),
-                                      data_frame_days)
+            data_frame_days.to_excel(writer, sheet_name=sheet_name, startrow=1, header=False)
+            self._set_workbook_layout(writer.book, (writer.sheets[sheet_name]), data_frame_days)
 
-        with pd.ExcelWriter("Hive Metrics.xlsx",
-                            engine="xlsxwriter",
-                            options={"strings_to_urls": False}) as writer:
+        with pd.ExcelWriter("Hive Metrics.xlsx", engine="xlsxwriter", options={"strings_to_urls": False}) as writer:
             workbook = writer.book
             worksheet = workbook.add_worksheet("Summary Charts")
             worksheet.hide_gridlines(2)
 
-            _insert_pie_chart(workbook,
-                              worksheet,
-                              title='New vs. Closed Cases',
-                              cell_pos='D2',
-                              series={
-                                  'name': 'Open vs. Closed Cases Last 30',
-                                  'categories': '=Tracking!$B$1:$C$1',
-                                  'values': '=Tracking!$B$2:$C$2',
-                              })
-            _insert_pie_chart(workbook,
-                              worksheet,
-                              title='Case Severities',
-                              cell_pos='M2',
-                              series={
-                                  'name': 'Severity Last 30',
-                                  'categories': '=Tracking!$A$8:$A$10',
-                                  'values': '=Tracking!$F$8:$F$10',
-                              })
-            _insert_pie_chart(workbook,
-                              worksheet,
-                              title='Case Resolution',
-                              cell_pos='D19',
-                              series={
-                                  'name': 'Case Resolution Last 30',
-                                  'categories': '=Tracking!$A$6:$A$7',
-                                  'values': '=Tracking!$E$6:$E$7',
-                              })
-            _insert_pie_chart(workbook,
-                              worksheet,
-                              title='Case Ownership',
-                              cell_pos='M19',
-                              series={
-                                  'name': 'Case Ownership Last 30',
-                                  'categories': '=Tracking!$A$3:$A$5',
-                                  'values': '=Tracking!$D$3:$D$5',
-                              })
+            _insert_pie_chart(
+                workbook,
+                worksheet,
+                title="New vs. Closed Cases",
+                cell_pos="D2",
+                series={
+                    "name": "Open vs. Closed Cases Last 30",
+                    "categories": "=Tracking!$B$1:$C$1",
+                    "values": "=Tracking!$B$2:$C$2",
+                },
+            )
+            _insert_pie_chart(
+                workbook,
+                worksheet,
+                title="Case Ownership",
+                cell_pos="M19",
+                series={
+                    "name": "Case Ownership Last 30",
+                    "categories": "=Tracking!$A$3:$A$9",
+                    "values": "=Tracking!$D$3:$D$9",
+                },
+            )
+            _insert_pie_chart(
+                workbook,
+                worksheet,
+                title="Case Resolution",
+                cell_pos="D19",
+                series={
+                    "name": "Case Resolution Last 30",
+                    "categories": "=Tracking!$A$10:$A$12",
+                    "values": "=Tracking!$E$10:$E$12",
+                },
+            )
+            _insert_pie_chart(
+                workbook,
+                worksheet,
+                title="Case Severities",
+                cell_pos="M2",
+                series={
+                    "name": "Severity Last 30",
+                    "categories": "=Tracking!$A$13:$A$15",
+                    "values": "=Tracking!$F$13:$F$15",
+                },
+            )
 
-            _data_frame_days_to_excel(writer,
-                                      sheet_name="Cases newer than 30 Days",
-                                      data_frame_days=self._data_frame_30days)
-            _data_frame_days_to_excel(writer,
-                                      sheet_name="Cases older than 60 days",
-                                      data_frame_days=self._data_frame_60days)
-            _data_frame_days_to_excel(writer,
-                                      sheet_name="Cases newer than 90 Days",
-                                      data_frame_days=self._data_frame_90days)
+            _data_frame_days_to_excel(
+                writer, sheet_name="Cases newer than 30 Days", data_frame_days=self._data_frame_30days,
+            )
+            _data_frame_days_to_excel(
+                writer, sheet_name="Cases older than 60 days", data_frame_days=self._data_frame_60days,
+            )
+            _data_frame_days_to_excel(
+                writer, sheet_name="Cases newer than 90 Days", data_frame_days=self._data_frame_90days,
+            )
 
             self._data_frame_counts.to_excel(writer, sheet_name="Tracking")
             writer.save()
@@ -276,17 +271,14 @@ class SIRPPipeline(object):
             SMTP_SERVER(str): mail server
         """
         msg = MIMEMultipart()
-        msg["From"] = "<FROM DOMAIN>"
+        msg["From"] = ""
         msg["To"] = SENT_TO
         msg["Subject"] = "The Hive Case Metrics"
-        msg.attach(
-            MIMEText(
-                "Attached are the requested case metrics in .XLSX format."))
+        msg.attach(MIMEText("Attached are the requested case metrics in .XLSX format."))
         part = MIMEBase("application", "octet-stream")
         part.set_payload(open("Hive Metrics.xlsx", "rb").read())
         encoders.encode_base64(part)
-        part.add_header("Content-Disposition",
-                        'attachment; filename="Hive Metrics.xlsx"')
+        part.add_header("Content-Disposition", 'attachment; filename="Hive Metrics.xlsx"')
         msg.attach(part)
         smtp = smtplib.SMTP(SMTP_SERVER)
         smtp.starttls()
