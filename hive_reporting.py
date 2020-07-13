6c08dff1a212f0797c404e851d48d5a2b7471794
Ryan Kelleher <5748165+RtKelleher@users.noreply.github.com>
2020-07-13 15:05:56 -0400

    Update hive_reporting.py
    remove todo

--- hive_reporting.py
+++ hive_reporting.py
@@ -6,9 +6,6 @@ Hive-Reporting provides easy to read case metrics supporting team contirubtions
 and frequency without the need to access or create custom report in
 The Hive Dashboard
 
-Todo:
-     * Remove manual aspect of Panda Charts
-
 """
 from __future__ import print_function
 from __future__ import unicode_literals
